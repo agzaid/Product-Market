333eea5c9ed1df4f1653077eee855d4649584f26
Ahmad Gamal <ahmad_zaid88@yahoo.com>
2020-11-14 21:38:33 +0200

    creating and lazy loading products module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
+import { ProductsModule } from './products/products.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from './material-module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
